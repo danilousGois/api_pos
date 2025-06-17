395f92c9fa91314c885b493c636d2fcebb47798a
Ronald-Kaue <133374965+Ronald-Kaue@users.noreply.github.com>
2025-06-17 14:12:58 +0000

    Excluindo arquivos desnecessários (⌐■_■)

--- messages.js
+++
@@ -1,9 +0,0 @@
-export const messages = {
-  invalidId: "O parâmetro 'id' deve ser um número válido.",
-  invalidContent: "O campo 'conteudo' é obrigatório e deve ser uma string.",
-  notFound: "Mensagem não encontrada. Verifique o ID e tente novamente.",
-  createError: "Erro ao criar a mensagem. Verifique os dados enviados.",
-  updateError: "Erro ao atualizar a mensagem. Tente novamente mais tarde.",
-  deleteError: "Erro ao deletar a mensagem. Tente novamente mais tarde.",
-  fetchError: "Erro interno ao buscar mensagens. Tente novamente mais tarde."
-}
